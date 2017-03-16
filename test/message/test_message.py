3e26643ce440106eb852402fa05fad6f28fdcbaa
abner_xin <346088968@qq.com>
2017-03-16 16:53:07 +0800

    enhance message case

--- test/message/test_message.py
+++ test/message/test_message.py
@@ -1,13 +1,9 @@
-import os
-import tempfile
 import unittest
 from message import IMSMessageParser
 from message import IMSMessage
+from email_resource import email_plain
+from email_resource import email_html_attachment
 
-
-email_path = os.path.dirname(os.path.abspath(__file__))
-email_plain = os.path.join(email_path, "email_plain.eml")
-email_html_attachment = os.path.join(email_path, "email_html_attachment.eml")
 
 
 class TestIMSMessageParser(unittest.TestCase):
@@ -32,12 +28,20 @@ class TestIMSMessage(unittest.TestCase):
         self.assertIn('test email', m.body_plain)
         self.assertIsNone(m.body_html)
 
+        self.assertTrue(m.is_header_value_contain("From", "sender@test.com"))
+        self.assertTrue(m.is_plain_body_contain("test email"))
+
     def test_parse_email_with_html_body_and_attachment(self):
         m = IMSMessageParser().message_from_file(email_html_attachment)
         self.assertEqual('plain and html body', m.subject)
         self.assertIn('hello world', m.body_plain)
         self.assertIn('<h1>hello world<h1>', m.body_html)
         self.assertDictEqual({'hello.txt': "hello"}, m.attachments)
+
+        self.assertTrue(m.is_html_body_contain("<h1>hello world<h1>"))
+        self.assertTrue(m.is_contain_attachment("hello.txt"))
+        self.assertFalse(m.is_contain_attachment("hello01.txt"))
+        self.assertTrue(m.is_attachment_contain("hello.txt", "hello"))
 
 
 if __name__ == '__main__':
